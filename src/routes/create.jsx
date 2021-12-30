487107ba753e43faf8e028ee2c8efe8b8d655751
kn-nalbantov <kn.nalbantov@gmail.com>
2021-12-30 10:15:10 +0200

    Added create form and basic styling

--- src/routes/create.jsx
+++ src/routes/create.jsx
@@ -1,7 +1,28 @@
 export default function Create() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Add a Destination</h2>
-      </main>
-    );
+  function onClick(e) {
+    e.target.nextElementSibling.focus();
   }
+
+  function onSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <main>
+      <div className='createBox'>
+        <form onSubmit={onSubmit}>
+          <h2>Add a Destination</h2>
+          <label htmlFor='name' onClick={onClick}>Name</label>
+          <input type='text' name='name' />
+          <label htmlFor='region' onClick={onClick}>Region</label>
+          <input type='text' name='region' />
+          <label htmlFor='img' onClick={onClick}>Select an image</label>
+          <input type='file' name='img' />
+          <label htmlFor='description' onClick={onClick}>Description</label>
+          <textarea name="description" cols="20" rows="5"></textarea>
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+    </main>
+  );
+}
