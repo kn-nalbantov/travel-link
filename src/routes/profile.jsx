201a51cb2d32e780e62606037d6c7006fc5e0f7f
kn-nalbantov <kn.nalbantov@gmail.com>
2022-01-04 18:15:07 +0200

    Added profile view for owner publications

--- src/routes/profile.jsx
+++ src/routes/profile.jsx
@@ -1,7 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { getDestinationsByOwnerId, getUserData } from '../api';
+
 export default function Profile() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Profile</h2>
-      </main>
-    );
+  const [destinations, setDestinations] = useState([]);
+  const userData = getUserData();
+  let ownerId;
+  if (userData) {
+    ownerId = userData.objectId;
   }
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await getDestinationsByOwnerId(ownerId);
+      // console.log(res)
+      setDestinations(res);
+    }
+    fetchData();
+  }, []);
+
+  // console.log(ownerId);
+  try{
+
+    console.log(destinations[0].attributes);
+  }catch{}
+
+  return (
+    <>
+      <h2 className="banner-header">My Publications</h2>
+      <main>
+        {destinations.map(x => (
+          <div className='travelCard' key={x.attributes.createdAt}>
+            <h2>{x.attributes.name}</h2>
+            <p>Region: {x.attributes.region}</p>
+            <img src={x.attributes.img._url} alt={x.attributes.img._name} />
+            <button className='detailsBtn'>Details</button>
+            <button className='editBtn'>Edit</button>
+          </div>
+        ))}
+      </main>
+    </>
+  );
+}
